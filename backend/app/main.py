9195d57d8ad69c0c60c50254091d1be4f9e34350
Kevin Jose M A <kjose569@gmail.com>
2025-07-30 19:06:57 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -149,5 +149,6 @@ async def hackrx_run(req: HackRxRequest):
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 7000))  # Default to 8000 locally
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
 
